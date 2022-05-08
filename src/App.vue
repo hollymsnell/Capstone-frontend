f5dfdea22fc02d64b0056211d1f014dae910f989
hollymsnell <hollymsnell@gmail.com>
2022-05-08 10:03:11 -0600

    create new exercise html set up and index create and show methods created

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <nav>
     <router-link to="/">Home</router-link>
     |
-    <router-link to="/about">About</router-link>
+    <router-link to="/signup">SignUp</router-link>
+    |
+    <router-link to="/login">Login</router-link>
   </nav>
   <router-view />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,61 @@
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "TherapE",
+      exercises: [],
+      className: "red",
+      newExercise: {},
+      currentExercise: {},
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexExercises();
+  },
+  methods: {
+    indexExercises: function () {
+      axios.get("/exercises").then((response) => {
+        console.log("exercises index", response);
+        this.exercises = response.data;
+      });
+    },
+    createExercise() {
+      axios.post("/exercises", this.newExercise).then((response) => {
+        this.exercises.push(response.data);
+      });
+    },
+    showExercise(exercise) {
+      axios.get(`/exercises/${exercise.id}`).then((response) => {
+        this.currentExercise = response.data;
+        document.querySelector("#exercise-details").showModal();
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <!-- <h2 v-bind:class="className">{{ exercises }} count: {{ exercises.length }}</h2> -->
+    <h2>Create New Exercise</h2>
+    <p>
+      Name:
+      <input type="text" v-model="newExercise.name" />
+    </p>
+    <p>
+      Instructions:
+      <input type="text" v-model="newExercise.instructions" />
+    </p>
+    <p>
+      Frequency:
+      <input type="text" v-model="newExercise.frequency" />
+    </p>
+    <p>
+      Image:
+      <input type="text" v-model="newExercise.image" />
+    </p>
   </div>
 </template>
 
