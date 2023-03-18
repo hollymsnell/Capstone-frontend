7bb6a669ce3f8c39d3c0dbd0e24fe54b03a4af90
hollymsnell <hollymsnell@gmail.com>
2023-03-18 11:34:48 -0600

    create exercise works now on new page but does not refesh to home

--- src/views/CreateExerciseView.vue
+++ src/views/CreateExerciseView.vue
@@ -4,7 +4,6 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      newExerciseParams: {},
       errors: [],
       newExercise: {},
       exercises: [],
@@ -14,7 +13,7 @@ export default {
   methods: {
     submit: function () {
       axios
-        .post("/add", this.newExerciseParams)
+        .post("/add", this.newExercise)
         .then((response) => {
           console.log(response.data);
           this.$router.push("/home");
@@ -51,6 +50,12 @@ export default {
       Image:
       <input type="text" v-model="newExercise.image" />
     </p>
+    <button class="btn btn-primary" v-on:click="createExercise()">Create</button>
+    <div v-for="exercise in exercises" v-bind:key="exercise.id">
+      <h3>{{ exercise.name }}</h3>
+      <p>{{ exercise.instructions }}</p>
+      <img v-bind:src="exercise.image" v-bind:alt="exercise.name" style="max-width: 250px" />
+    </div>
   </div>
 </template>
 
